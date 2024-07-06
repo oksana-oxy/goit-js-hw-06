07d9bc9537d9d93050dfb0c83de538718a0671b5
oksana-oxy <pivenoxana143@gmail.com>
2024-07-06 22:48:58 +0300

    commit version1

---
+++ js/task-2.js
@@ -0,0 +1,37 @@
+class Storage {
+    #items;
+    constructor(items) {
+        this.#items = items;
+    }
+    get items() {
+        return this.#items;
+    }
+    set items(newItems) {
+        this.#items = newItems;
+    }
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+    removeItem(itemToRemove) {
+        //this.#items = this.#items.filter(item => item !== itemToRemove);
+        const index = this.#items.indexOf(itemToRemove);
+        if (index !== -1) {
+            this.#items.splice(index, 1);
+        }
+    }
+}
+    const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
